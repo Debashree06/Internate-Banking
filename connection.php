f5fe38ffa627eaea43388c5f072e1d11ccc34af2
srinatharitgithub <107142549+srinatharitgithub@users.noreply.github.com>
2022-07-12 12:52:14 +0530

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php
+$user name = $p_post['user name'];
+$email = $p_post['email'];
+$mobile.No= $p_post['mobile.No'];
+$password = $p_post['password'];
+$confirm password = $p_post['confirm password'];
+$conn=new mysqli('localhost','',;,'registation');
+if($conn->connected_error){
+    die ('connection failed :'.$conn->connected_error);
+}else{
+    $stmt = ->prepair("insert into registation (username,email,mobile.No,password,confirmpassword)values(?,?,?,?,?)");
+    $stmt->bind_parm("ssiss",$username,$email,$mobile.No,$password$confirmpassword);
+    $stmt->execute();
+    echo"registation sucessful...";
+    $stmt->close();
+    $conn->close();
+}
+?>
